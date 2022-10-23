487e2fc2230720b9396228c555724be3eb372e80
Hwangjo Kim <hwangjo95@kakao.com>
2022-10-23 00:45:50 -0700

    Change login algorithm Still need to handle errors for login and update userData

--- src/LoginApp.java
+++ src/LoginApp.java
@@ -1,61 +1,52 @@
-import java.util.Scanner;
 
-/**
- * Lead Author(s):
- * 
- * @author Christopher Dove
- * @author Jo Kim
- *         <<add additional lead authors here, with a full first and last name>>
- * 
- *         Other contributors:
- *         <<add additional contributors (mentors, tutors, friends) here, with
- *         contact information>>
- * 
- *         References:
- *         Morelli, R., & Walde, R. (2016). Java, Java, Java: Object-Oriented
- *         Problem Solving.
- *         Retrieved from
- *         https://open.umn.edu/opentextbooks/textbooks/java-java-java-object-oriented-problem-solving
- * 
- *         <<add more references here>>
- * 
- *         Version/date: 10/14/2022
- * 
- *         Responsibilities of class: To create accounts and access them for the casino game
- * 
- */
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
+
+import javax.annotation.processing.FilerException;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import java.io.PrintWriter;
 public class LoginApp {
 
     //FIELD
-  private static int usernumber = 0; // track PLYER number
- 
+private static Scanner userInput = new Scanner(System.in);
+private static String inputId, inputPassword;
+private static int inputChips;
+private static int inputMoney = 1000;
+public static List<String> userDataList = new ArrayList<String>();
+// private static Player playerData = new Player(Casino.getPlayer().getMoney(), Casino.getPlayer().getChips(), Casino.getPlayer().getPassword(), Casino.getPlayer().getUsername());
+private static File txtFile = new File("userData.txt");
 
-  private static Player players[] = new Player[20];  //MAX player is 20
-  private static Scanner userInput = new Scanner(System.in);
-  private static String inputId, inputPassword, inputUsername;
-  
+// private static String userData;
 
+  /**
+   * The main function of the program.1
+   * 
+   * @param args The command line arguments.
+   */
   public static void main(String[] args) {
-    for(int i = 0; i < 20; i++){
-        players[i] = new Player(); //
-    }
 
         boolean runState = true;
         
         do{
             System.out.println("Choose menu");
-            System.out.println("1. Create account  \n2. Login  \n3. Delete Account \n4. Enter to Casino");
+            System.out.println("1. Create account  \n2. Login  \n3. Delete Account \n4. Return To Casino");
             
             int select = userInput.nextInt();
             
             switch(select){
                 
                 case 1:
-                    if(usernumber == 20){
-                        System.out.println("User ID is full");
-                        break;
-                    }
-                    else 
                     creatAccount();
                     break;
 
@@ -63,13 +54,14 @@ public class LoginApp {
                     loginAccount();
                     break;
                 
-                case 3:
-                    deleteAccount();
-                    break;
+                // case 3:
+                //     deleteAccount();
+                //     break;
 
                 case 4:
                     Casino.main(args);
                     break;
+
 
                 default:
                 System.out.println("Select a menu option by entering a value from 1 to 4.");
@@ -83,73 +75,222 @@ public class LoginApp {
     }  
 
 
-    private static void creatAccount() {
+    /**
+     * Creates a new account for the player.
+     */
+    public static void creatAccount() {
+        
+		boolean validId = false;
+		do {
+            System.out.println("Enter your ID");
+            inputId = userInput.next();
+            
+            if (inputId != null) {
+            	Casino.getPlayer().setId(inputId);
+            	validId = true;
+            } else continue;
+            
+            System.out.println("Enter Password");
+            inputPassword = userInput.next();
+            Casino.getPlayer().setPassword(inputPassword);
+            
+		} while (validId == false);
+       
+        /**
+         * Writes the user's information to the userData.txt.
+         * @param inputId The user's ID.
+         * @param inputPassword The user's password.
+         */
+        try {
+                FileWriter myWriter = new FileWriter("userData.txt", true);
+                PrintWriter myPrintWriter = new PrintWriter(myWriter);
+                
+                myPrintWriter.println(inputId + " " + inputPassword + " "  + inputChips + " "  + inputMoney);
 
-        System.out.println("Enter your ID");
-        inputId = userInput.next();
-        players[usernumber].setId(inputId);
-        
-        
-        System.out.println("Enter Password");
-        inputPassword = userInput.next();
-        players[usernumber].setPassword(inputPassword);
-        
-        
-        System.out.println("Enter User Name");
-        inputUsername = userInput.next();
-        players[usernumber].setUsername(inputUsername);
-        
+                System.out.println("-----------------------------");
+                System.out.println("You've successfully created account!");
+                System.out.println("-----------------------------");
 
-        usernumber++; //to track 20 user
+                myPrintWriter.close();
+                myWriter.close();
+                setAccount(Casino.getPlayer(), inputId, inputPassword, inputChips, inputMoney);
+
+        } catch(IOException e) {
+                System.out.println("An error occurred");
+                e.printStackTrace();
+            }
         
     }
+    
+    private static void setAccount(Player player, String inputId, String inputPassword, int inputChips, int inputMoney)
+    {
+        player.setUsername(inputId);
+        player.setPassword(inputPassword);
+        player.setChips(inputChips);
+        player.setMoney(inputMoney);
+        // TODO maybe have to add setChips and setMoney, test more
+    }
+    
+    public static void updateAccount(Player player, String userId, String userPassword, int userChip, int userMoney) 
+    {
+        player.setId(userId);;
+        player.setPassword(userPassword);
+        player.setChips(userChip);
+        player.setMoney(userMoney);
 
+        boolean updateAccount  = false;
+
+        do {
+            
+            try {
+                String line = null;
+                FileReader fileReader = new FileReader(txtFile);
+                List<String> userDataList = new ArrayList<String>();
+                FileWriter myWriter = new FileWriter("userData.txt", true);
+                PrintWriter myPrintWriter = new PrintWriter(myWriter);
+                BufferedReader bReader = new BufferedReader(fileReader);
+
+                //Create ArrayList of user Data [userID, userPassword, userChip, userCash]
+                while ((line = bReader.readLine()) != null)
+                {   
+                    userDataList.add(line);
+                }    
+                
+                for (String userData : userDataList) {
+                    
+                    if  ((userData.contains(userId)))
+                    { 
+                        myPrintWriter.println(userId + " " + userPassword + " "  + userChip + " "  + userMoney);
+                        System.out.println("---------------------------------");
+                        System.out.println("Successfully Updated Account");
+                        System.out.println("---------------------------------");
+                        myPrintWriter.close();
+                        bReader.close();
+                        updateAccount = true;
+        
+                    } else{
+                        System.out.println("---------------------------------");
+                        System.out.println("Please Enter Valid ID and Password");
+                        System.out.println("---------------------------------");
+                        return;
+                    }
+                }
+            
+                
+                    
+          
+
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        
+
+        
+        }while(updateAccount != true);
+    
+    }
+    /**
+     *
+     *
+     * Logs in the account.
+     * 
+     * @return Whether the login was successful.
+     */
     private static void loginAccount() {
 
-        System.out.println("Enter your ID");
-        inputId = userInput.next();
-        System.out.println("Enter Password");
-        inputPassword = userInput.next();
-
-        int loginSuccess = 0;
-        for(int i = 0; i < players.length; i++){
-            if (inputId.equals(players[i].getId()) && inputPassword.equals(players[i].getPassword()))
-            {
-                System.out.println("Successfully login to your account");
-                System.out.println("Hi " + players[i].getUsername());
-                players[i].setId(inputId);
-                players[i].setPassword(inputPassword);
-                loginSuccess = 1;
-                break;
-            }
-        if(loginSuccess == 0){
-            System.out.println("Wrong userID and Password");
-            }
-        }
+        /**
+         * Checks if the input ID and password are valid.
+         * Checks for a matching user ID and password in the text file, login if they match
+         * @param inputId The ID of the player.
+         * @param inputPassword The password of the player.
+         * @return Whether the ID and password are valid.
+         */
         
+        // searching algorithim for inputId in userData.txt
+        // if not found return invalid id
+        // if found check if line below contains correct password
+        // if both true login account
+        boolean loginSuccess  = false;
+
+        do {
+            
+            try {
+                String line = null;
+                FileReader fileReader = new FileReader(txtFile);
+                BufferedReader bReader = new BufferedReader(fileReader);
+                
+                //Create ArrayList of user Data [userID, userPassword, userChip, userCash]
+                while ((line = bReader.readLine()) != null)
+                {   
+                    userDataList.add(line);
+                    System.out.println(userDataList);
+                }
+                
+                //userInput
+                System.out.println("Enter your ID");
+                inputId = userInput.next();
+                System.out.println("Enter Password");
+                inputPassword = userInput.next();
+                
+                
+
+                for (String userData : userDataList) {
+                    
+                    if  ((userData.contains(inputId)) && (userData.contains(inputPassword)))
+                        { 
+                            //Change userDataList to String Array
+                            String[] userDataString = userDataList.toArray(new String[3]);
+                            String splitUserData = userDataString[0];
+
+                            //Change userData String Array to String
+                            userDataString = splitUserData.split(" ");
+                            
+                            //Set Player data from userData.txt
+                            final String userId = userDataString[0];
+                            final String userPassword = userDataString[1];
+                            final int userChip = Integer.valueOf(userDataString[2]);
+                            final int userCash = Integer.valueOf(userDataString[3]);
+    
+                            System.out.println("---------------------------------");
+                            System.out.println("Login Success");
+                            System.out.println("---------------------------------");
+                            setAccount(Casino.getPlayer(), userId, userPassword, userChip, userCash);
+                            bReader.close();
+                            loginSuccess = true;
+    
+                        } else {    
+                            System.out.println("---------------------------------");
+                            System.out.println("Please Enter Valid userID and Password");
+                            System.out.println("---------------------------------");
+
+                            return;
+                        }
+                }
+               
+                
+                    
+            } catch (FileNotFoundException e) {
+                 // TODO Auto-generated catch block
+                 e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        } while (loginSuccess != true);
     }
-    private static void deleteAccount() {
-        System.out.println("Enter your ID");
-        inputId = userInput.next();
-        System.out.println("Enter Password");
-        inputPassword = userInput.next();
-        int deleteSuccess = 0;
 
-        for (int i = 0; i < players.length; i++) {
-            if (inputId.equals(players[i].getId()) && inputPassword.equals(players[i].getPassword())) {
-                
-                System.out.println("Succesefully Deleted User Account");
-                players[i].setId(null);
-                players[i].setPassword(null);
-                players[i].setUsername(null);
-                usernumber--; //decrease user Number
-                deleteSuccess = 1;
-            }
-            if (deleteSuccess == 0) {
-
-                System.out.println("No other account");
-                
-            }
-        }
+    public static boolean checkId() {
+        boolean accountReal = true;
+         
+        // TODO user has to enter a username with at least 1 character 
+        if (Casino.getPlayer().getId().equals(null))
+        {
+            accountReal = false;
+        } 
+         
+        return accountReal;
+        
     }
 }
