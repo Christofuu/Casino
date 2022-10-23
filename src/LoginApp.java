693c038f5e67a83c28d5eadc8d0ce3540e30f461
Hwangjo Kim <hwangjo95@kakao.com>
2022-10-23 13:02:27 -0700

    LoginApp changed

--- src/LoginApp.java
+++ src/LoginApp.java
@@ -1,11 +1,7 @@
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Random;
 import java.util.Scanner;
-import java.util.Set;
-
 import javax.annotation.processing.FilerException;
 
 import java.io.BufferedReader;
